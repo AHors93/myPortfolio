779d6a70b98648751f07367dc8b9ff5b3985bb89
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-09-20 11:26:37 +0100

    Updates

---
+++ precache-manifest.44b1aaa5350af2a89b23ffbd70fd148e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a16229de5bf6d48ca4c02b14dbd605f",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "756c1910a7bf797abfaf",
+    "url": "/myPortfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3720609fd85f25a83e6f",
+    "url": "/myPortfolio/static/css/main.37892d08.chunk.css"
+  },
+  {
+    "revision": "756c1910a7bf797abfaf",
+    "url": "/myPortfolio/static/js/2.78c96b40.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/myPortfolio/static/js/2.78c96b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3720609fd85f25a83e6f",
+    "url": "/myPortfolio/static/js/main.0c2936b5.chunk.js"
+  },
+  {
+    "revision": "5694beadf6b5b89a5d41",
+    "url": "/myPortfolio/static/js/runtime-main.25349af6.js"
+  }
+]);
